5349dc7f398f88d425459715865da20ff9d24669
AlexPigalyov <alex.pigalyov@gmail.com>
2021-02-10 17:43:07 +0300

    add dpdcityrepository + connect entity framework

---
+++ Web-Api.online/Repositories/DpdCityRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Web_Api.online.Models.Tables.DPDCities;
+
+namespace Web_Api.online.Repositories
+{
+    public static class DpdCityRepository
+    {
+        public static async Task<dynamic> GetDPDCityAsync(string cityName)
+        {
+            using (var ctx = new webapionlineContext())
+            {
+                var top10cities = await ctx.DpdCities.FromSqlRaw($"spDPDCitiesTop10 '{cityName}'").ToListAsync();
+
+                return top10cities;
+            }
+        }
+    }
+}
