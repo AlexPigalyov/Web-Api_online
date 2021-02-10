ae0dc28d677fa9be130244486406bcc3118f5d70
AlexPigalyov <alex.pigalyov@gmail.com>
2021-02-10 15:51:03 +0300

    first test commit

---
+++ Web-Api.online/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Api.online.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok("123");
+        }
+    }
+}
