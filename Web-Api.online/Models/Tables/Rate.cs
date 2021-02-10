f4ed5651839837507393d3cc28b3f8508b3c5a00
AlexPigalyov <alex.pigalyov@gmail.com>
2021-02-10 20:35:52 +0300

    add currencies controller

---
+++ Web-Api.online/Models/Tables/Rate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Web_Api.online.Models.Tables
+{
+    public partial class Rate
+    {
+        public long Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Web-Api.online/Repositories/RateRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Web_Api.online.Models.Tables;
+
+namespace Web_Api.online.Repositories
+{
+    public static class Raterepository
+    {
+        public static Rate GetLastRates()
+        {
+            using (var ctx = new webapionlineContext())
+            {
+                return ctx.Rates.OrderByDescending(item => item.Id).FirstOrDefault();
+            }
+        }
+    }
+}
