70322e5a0a2134b5403d00dd237d255a0f231ed5
AlexPigalyov <alex.pigalyov@gmail.com>
2021-02-10 19:10:45 +0300

    add dpd cities delivery cost controller

---
+++ Web-Api.online/Models/DPDCalcRequestModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Web_Api.online.Models
+{
+    public class DPDCalcRequestModel
+    {
+        public string ClientKey { get; set; }
+        public long ClientNumber { get; set; }
+        public string DeliveryCityName { get; set; }
+        public string PickupCityName { get; set; }
+        public bool SelfPickup { get; set; }
+        public bool SelfDelivery { get; set; }
+        public double DeclaredValue { get; set; }
+        public double Weight { get; set; }
+    }
+}
