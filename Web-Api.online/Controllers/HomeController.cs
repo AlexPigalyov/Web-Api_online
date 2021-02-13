244ad3cc5444a2ca55490d4e702b5afc7cced3dc
Rentoolo Planet <mathewsun@rambler.ru>
2021-02-14 01:22:02 +0300

    Update HomeController.cs

--- Web-Api.online/Controllers/HomeController.cs
+++ Web-Api.online/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace Web_Api.online.Controllers
         [HttpGet]
         public IActionResult Test()
         {
-            return Ok("123456789012");
+            return Ok("1234567890123");
         }
     }
 }
