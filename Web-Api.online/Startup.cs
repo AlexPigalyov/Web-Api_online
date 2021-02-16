d7f81f748fa1404087b7ba88663000473358ee88
AlexPigalyov <alex.pigalyov@gmail.com>
2021-02-16 22:27:13 +0300

    add cors

--- Web-Api.online/Startup.cs
+++ Web-Api.online/Startup.cs
@@ -36,6 +36,12 @@ namespace Web_Api.online
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
