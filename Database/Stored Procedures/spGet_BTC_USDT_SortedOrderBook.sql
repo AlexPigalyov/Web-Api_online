e5a0cbe1b2fd19c3199ee46c30865b603f342db0
Alex <alex.pigalyov@gmail.com>
2021-08-07 00:47:12 +0300

    edit the stored procedure

--- Database/Stored Procedures/spGet_BTC_USDT_SortedOrderBook.sql
+++ Database/Stored Procedures/spGet_BTC_USDT_SortedOrderBook.sql
@@ -1,13 +1,23 @@
-CREATE PROCEDURE [dbo].[spGet_BTC_USDT_SortedOrderBook]
+USE [Exchange]
+GO
+/****** Object:  StoredProcedure [dbo].[spGet_BTC_USDT_SortedOrderBook]    Script Date: 06.08.2021 23:37:52 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[spGet_BTC_USDT_SortedOrderBook]
+@isBuy bit
 AS
 BEGIN
 
-SELECT DISTINCT COUNT(D1.Price) AS CountPrices, D1.Price,
+SELECT DISTINCT COUNT(D1.Price) AS CountPrices, D1.Price, D1.IsBuy,
     (SELECT SUM(D2.Amount)
     FROM [Exchange].[dbo].[BTC_USDT_OpenOrders] AS D2
     WHERE D2.Price = D1.Price) AS Amount
 FROM [Exchange].[dbo].[BTC_USDT_OpenOrders] AS D1
-GROUP BY  D1.Price
+WHERE D1.IsBuy = @isBuy
+GROUP BY  D1.Price, D1.IsBuy
 ORDER BY  Price
+
 
 END
