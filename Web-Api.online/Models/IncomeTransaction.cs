d75f081f90edc907eb9681673f169af4859b5f86
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-08-16 20:15:19 +0300

    add model IncomeTransaction

---
+++ Web-Api.online/Models/IncomeTransaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Web_Api.online.Models
+{
+    public class IncomeTransaction
+    {
+        public long Id { get; set; }
+        public string CurrencyAcronim { get; set; }
+        public string TransactionId { get; set; }
+        public decimal Amount { get; set; }
+        public decimal TransactionFee { get; set; }
+        public string ToAddress { get; set; }
+        public decimal Date { get; set; }
+        public string UserId { get; set; }
+        public int WalletId { get; set; }
+    }
+}
