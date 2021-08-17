20ddccb2cd2fe4684d397aaff2ddac95c9f28be7
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-08-17 07:00:21 +0300

    delete value

--- Web-Api.online/Models/IncomeWallet.cs
+++ Web-Api.online/Models/IncomeWallet.cs
@@ -10,7 +10,6 @@ namespace Web_Api.online.Models
         public int Id { get; set; }
         public string UserId { get; set; }
         public string CurrencyAcronim { get; set; }
-        public decimal Value { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastUpdate { get; set; }
         public string Address { get; set; }
