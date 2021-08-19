00a388367fe018e56b88a789b0ca3bac7c9158a6
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-08-19 21:56:41 +0300

    add convert to decimal

---
+++ Web-Api.online/Extensions/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Web_Api.online.Extensions
+{
+    public static class StringExtensions
+    {
+        public static decimal? ConvertToDecimal(this string number)
+        {
+            decimal num;
+            try
+            {
+                number = number.Replace(".", ",");
+                num = Convert.ToDecimal(number);
+            }
+            catch
+            {
+                try
+                {
+                    number = number.Replace(",", ".");
+                    num = Convert.ToDecimal(number);
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            return num;
+        }
+    }
+}
