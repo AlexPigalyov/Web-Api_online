3e6794a0e70b1dc721a4795d402c97835725695a
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-08-20 22:28:16 +0300

    fix eventTypeEnum

--- Web-Api.online/Models/Enums/EventTypeEnum.cs
+++ Web-Api.online/Models/Enums/EventTypeEnum.cs
@@ -3,9 +3,14 @@
     public enum EventType
     {
         Registration = 1,
-        Income = 2,
-        Outcome = 3,
-        Send = 4,
-        Recieve = 5
+        Income = 10000000,
+        IncomeLTC,
+        Outcome = 20000000,
+        OutcomeLTC,
+        Send = 30000000,
+        SendLTC,
+        Recieve = 40000000,
+        RecieveLTC,
+        Create = 50000000,
     }
 }
