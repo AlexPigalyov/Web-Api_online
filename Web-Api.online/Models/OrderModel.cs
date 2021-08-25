8221417f985d742a6709acdabed2e2e485bb32f2
Alex <alex.pigalyov@gmail.com>
2021-08-25 15:36:23 +0300

    add h8ubcontext + OrderModel.cs

---
+++ Web-Api.online/Models/OrderModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_Api.online.Models
+{
+    public class OrderModel
+    {
+        public decimal Price { get; set; }
+        public decimal Amount { get; set; }
+        public bool IsBuy { get; set; }
+    }
+}
