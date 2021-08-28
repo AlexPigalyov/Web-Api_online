64bdb8206379b7f654e95389e40df58378ceb9d3
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-08-28 17:57:05 +0300

    fix SendCoinsModel

--- Web-Api.online/Models/SendCoinsModel.cs
+++ Web-Api.online/Models/SendCoinsModel.cs
@@ -4,9 +4,8 @@ namespace Web_Api.online.Models
 {
     public class SendCoinsModel
     {
-        public Wallet WalletSender { get; set; }
-        public Wallet WalletReceiver { get; set; }
         public Events EventSender { get; set; }
         public Events EventReceiver { get; set; }
+        public Transfer Transfer { get; set; }
     }
 }
