db29d8d398f90f0c1ec3a49fc24b43a51980a458
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-08-28 14:12:30 +0300

    add SendCoinsModel

---
+++ Web-Api.online/Models/SendCoinsModel.cs
@@ -0,0 +1,12 @@
+﻿using Web_Api.online.Models.Tables;
+
+namespace Web_Api.online.Models
+{
+    public class SendCoinsModel
+    {
+        public Wallet WalletSender { get; set; }
+        public Wallet WalletReceiver { get; set; }
+        public Events EventSender { get; set; }
+        public Events EventReceiver { get; set; }
+    }
+}
