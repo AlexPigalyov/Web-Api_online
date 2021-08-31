06ce2c95c2bb0aa3b9966d5040c7d65fe95bd0d2
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-09-01 00:06:19 +0300

    add models

---
+++ Web-Api.online/Models/WithdrawModels/GeneralWithdrawModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_Api.online.Models.WithdrawModels
+{
+    public class GeneralWithdrawModel
+    {
+        public GeneralWithdrawModel()
+        {
+
+        }
+        public GeneralWithdrawModel(string currency)
+        {
+            Currency = currency;
+            AmountMin = 777;
+            Commission = 777;
+        }
+
+        public string Status { get; set; }
+        public decimal AmountMin { get; set; }
+        public decimal Commission { get; set; }
+
+        [Required]
+        public string Currency { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string Amount { get; set; }
+        public string Comment { get; set; }
+    }
+}
