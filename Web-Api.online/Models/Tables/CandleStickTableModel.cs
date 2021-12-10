24caa27eecf60520508f3acebf61ee7162e5b916
Alex <alex.pigalyov@gmail.com>
2021-12-11 02:37:04 +0300

    add to CandleStickTableModel TimeStamp properties

--- Web-Api.online/Models/Tables/CandleStickTableModel.cs
+++ Web-Api.online/Models/Tables/CandleStickTableModel.cs
@@ -6,9 +6,11 @@ namespace Web_Api.online.Models.Tables
     {
         public decimal Close { get; set; }
         public DateTime CloseTime { get; set; }
+        public long CloseTimeStamp => ((DateTimeOffset)CloseTime).ToUnixTimeSeconds();
         public decimal High { get; set; }
         public decimal Low {  get; set; }
         public decimal Open { get; set; }
         public DateTime OpenTime { get; set;  }
+        public long OpenTimeStamp => ((DateTimeOffset)OpenTime).ToUnixTimeSeconds();
     }
 }
