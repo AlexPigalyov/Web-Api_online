4fcd722232bf4b928cf9bb5d197a7f4cc8ae55c9
matthew <mathew.dubrovin@gmail.com>
2021-12-11 19:13:47 +0300

    fim candlestick model timestamp for datetime min values

--- Web-Api.online/Models/Tables/CandleStickTableModel.cs
+++ Web-Api.online/Models/Tables/CandleStickTableModel.cs
@@ -6,11 +6,11 @@ namespace Web_Api.online.Models.Tables
     {
         public decimal Close { get; set; }
         public DateTime CloseTime { get; set; }
-        public long CloseTimeStamp => ((DateTimeOffset)CloseTime).ToUnixTimeSeconds();
+        public long CloseTimeStamp => CloseTime > DateTime.MinValue ? ((DateTimeOffset)CloseTime).ToUnixTimeSeconds() : 0;
         public decimal High { get; set; }
-        public decimal Low {  get; set; }
+        public decimal Low { get; set; }
         public decimal Open { get; set; }
-        public DateTime OpenTime { get; set;  }
-        public long OpenTimeStamp => ((DateTimeOffset)OpenTime).ToUnixTimeSeconds();
+        public DateTime OpenTime { get; set; }
+        public long OpenTimeStamp => OpenTime > DateTime.MinValue ? ((DateTimeOffset)OpenTime).ToUnixTimeSeconds() : 0;
     }
 }
