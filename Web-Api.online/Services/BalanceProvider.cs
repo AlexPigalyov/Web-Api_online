103c5d0b3d5a2b490ef5d59146cb89f488944eae
matthew <mathew.dubrovin@gmail.com>
2021-12-27 21:13:22 +0000

    update BalanceProvider

---
+++ Web-Api.online/Services/BalanceProvider.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Web_Api.online.Data.Repositories;
+using Web_Api.online.Models;
+using Web_Api.online.Models.Enums;
+using Web_Api.online.Models.Tables;
+
+namespace Web_Api.online.Services
+{
+    public class BalanceProvider
+    {
+        private WalletsRepository walletsRepository;
+
+        public BalanceProvider(WalletsRepository walletsRepository)
+        {
+            this.walletsRepository = walletsRepository;
+        }
+
+
+        public async Task<BalanceProviderModel> Income(WalletTableModel walletTableModel,
+            IncomeTransactionTableModel incomeTransaction)
+        {
+            var currencyTableModel = await walletsRepository.GetCurrencyByAcronimAsync(incomeTransaction.CurrencyAcronim);
+
+            var balanceProviderModel = new BalanceProviderModel();
+            balanceProviderModel.PercentCommission = currencyTableModel.PercentCommissionForIncomeTransaction;
+            balanceProviderModel.StartBalance = walletTableModel.Value;
+
+            if (currencyTableModel.PercentCommissionForIncomeTransaction != null)
+            {
+                balanceProviderModel.Commission = incomeTransaction.Amount * currencyTableModel.PercentCommissionForIncomeTransaction.Value;
+            }
+            else
+            {
+                balanceProviderModel.ResultBalance = walletTableModel.Value + incomeTransaction.Amount;
+            }
+
+            return balanceProviderModel;
+        }
+    }
+}
