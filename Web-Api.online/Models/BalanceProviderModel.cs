f6f544c8514038c12220f124d5d6c1ac5d058a49
matthew <mathew.dubrovin@gmail.com>
2021-12-27 21:13:02 +0000

    add BalanceProviderModel

---
+++ Web-Api.online/Models/BalanceProviderModel.cs
@@ -0,0 +1,12 @@
+﻿using Web_Api.online.Models.Tables;
+
+namespace Web_Api.online.Models
+{
+    public class BalanceProviderModel
+    {
+        public decimal? PercentCommission { get; set; }
+        public decimal? Commission { get; set; }
+        public decimal StartBalance { get; set; }
+        public decimal ResultBalance { get; set; }
+    }
+}
