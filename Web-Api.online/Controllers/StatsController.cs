b1f31e5db035fc9b49ef0bfbe4c3732c7336cc6c
0takan <hordienko30@gmail.com>
2022-01-08 14:52:50 +0200

    Added new Controllers/StatsController.cs

--- Web-Api.online/Controllers/StatsController.cs
+++ Web-Api.online/Controllers/StatsController.cs
@@ -1,10 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Web_Api.online.Data.Repositories;
+using Web_Api.online.Models.Tables;
 
 namespace Web_Api.online.Controllers
 {
     public class StatsController : Controller
     {
+        private TransactionsRepository _transactionsRepository;
+
+        public StatsController(TransactionsRepository transactionsRepository)
+        {
+            _transactionsRepository = transactionsRepository;
+        }
+
+        public async Task<ActionResult> Incomings()
+        {
+            List<IncomeTransactionTableModel> incomes = await _transactionsRepository.GetAllIncomeTransactions();
+
+            return View(incomes);
+        }
+
         public ActionResult Index()
         {
             return View();
