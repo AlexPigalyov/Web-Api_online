ff43268504c22e2ae6e9d17b60732466fa51902d
0takan <hordienko30@gmail.com>
2022-01-08 17:22:32 +0200

    Added New view Transfers, added new element of Controllers/StatsController.cs, update TransferRepository and added new href in Index.cshtml

--- Web-Api.online/Controllers/StatsController.cs
+++ Web-Api.online/Controllers/StatsController.cs
@@ -10,10 +10,12 @@ namespace Web_Api.online.Controllers
     public class StatsController : Controller
     {
         private TransactionsRepository _transactionsRepository;
+        private TransferRepository _transfer;
 
-        public StatsController(TransactionsRepository transactionsRepository)
+        public StatsController(TransactionsRepository transactionsRepository, TransferRepository transfer)
         {
             _transactionsRepository = transactionsRepository;
+            _transfer = transfer;
         }
 
         public async Task<ActionResult> Incomings()
@@ -21,6 +23,12 @@ namespace Web_Api.online.Controllers
             List<IncomeTransactionTableModel> incomes = await _transactionsRepository.GetAllIncomeTransactions();
 
             return View(incomes);
+        }
+        public async Task<ActionResult> Transfers()
+        {
+            List<TransferTableModel> transfers = await _transfer.GetAllTransfers();
+
+            return View(transfers);
         }
 
         public ActionResult Index()
