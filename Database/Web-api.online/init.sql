72a066472fd348aaf39902dd4c386ed0fb4866ad
0takan <hordienko30@gmail.com>
2022-01-18 22:22:44 +0200

    commit

--- Database/Web-api.online/init.sql
+++ Database/Web-api.online/init.sql
@@ -209,53 +209,6 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[StudentsInfo](
-	[StudentId] [bigint] IDENTITY(1,1) NOT NULL,
-	[FistName] [nvarchar](50) NOT NULL,
-	[LastName] [nvarchar](50) NOT NULL,
-	[DateBorn] [date] NOT NULL,
-	[Course] [int] NOT NULL,
-	[Faculty] [nvarchar](50) NOT NULL,
- CONSTRAINT [PK_StudentsInfo] PRIMARY KEY CLUSTERED 
-(
-	[StudentId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[TeachersAndLessons](
-	[TeachId] [bigint] NOT NULL,
-	[LessonName] [nvarchar](30) NOT NULL,
- CONSTRAINT [PK_TeachersAndLessons] PRIMARY KEY CLUSTERED 
-(
-	[TeachId] ASC,
-	[LessonName] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[TeachersInfo](
-	[TeacherId] [bigint] IDENTITY(1,1) NOT NULL,
-	[FirstName] [nvarchar](50) NOT NULL,
-	[LastName] [nvarchar](50) NOT NULL,
-	[Lessons] [nvarchar](50) NOT NULL,
-	[Description] [nvarchar](600) NOT NULL,
- CONSTRAINT [PK_TeachersInfo] PRIMARY KEY CLUSTERED 
-(
-	[TeacherId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[UsersInfo](
 	[UserId] [nvarchar](450) NOT NULL,
 	[ProfilePhotoPath] [nvarchar](max) NULL,
