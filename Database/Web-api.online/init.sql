1bce2071fc3ad002c6867cefe7f8ed14ce33b44a
0takan <hordienko30@gmail.com>
2022-02-20 18:34:19 +0200

    Created GerUserRefferals proc and updated init.sql

--- Database/Web-api.online/init.sql
+++ Database/Web-api.online/init.sql
@@ -717,6 +717,33 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+CREATE PROCEDURE [dbo].[GetUserRefferals]
+@userId nvarchar(450)
+
+AS
+BEGIN
+--declare @userId nvarchar(450);
+--set @userId = '08d803ba-a9fb-430e-a0b9-d4a366aeaee7'
+
+
+SELECT 
+anu.Email,
+anu.UserName,
+ui.FullName,
+ui.RefferalId
+
+
+FROM AspNetUsers as anu
+LEFT JOIN UsersInfo as ui
+ON anu.Id = ui.UserId
+WHERE ui.RefferalId = @userId
+
+END
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE PROCEDURE [dbo].[GetUsers_Paged]
 @page int,
 @pageSize int
