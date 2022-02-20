56fe9bdb76ec1f842f7e8f515fdfb35b7804a0db
0takan <hordienko30@gmail.com>
2022-02-20 20:02:10 +0200

    Add GetAllRefferals.sql proc and updated init.sql

---
+++ Database/Web-api.online/Stored Procedures/GetAllRefferals.sql
@@ -0,0 +1,24 @@
+USE [web-api.online]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[GetAllRefferals]
+AS
+BEGIN
+
+SELECT 
+anu.Email,
+anu.UserName,
+ui.FullName,
+ui.ReffererId
+
+
+FROM AspNetUsers as anu
+LEFT JOIN UsersInfo as ui
+ON anu.Id = ui.UserId
+WHERE ui.ReffererId IS NOT NULL
+
+END
--- Database/Web-api.online/init.sql
+++ Database/Web-api.online/init.sql
@@ -428,6 +428,28 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+CREATE PROCEDURE [dbo].[GetAllRefferals]
+AS
+BEGIN
+
+SELECT 
+anu.Email,
+anu.UserName,
+ui.FullName,
+ui.ReffererId
+
+
+FROM AspNetUsers as anu
+LEFT JOIN UsersInfo as ui
+ON anu.Id = ui.UserId
+WHERE ui.ReffererId IS NOT NULL
+
+END
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE PROCEDURE [dbo].[GetAllRegistratedUser]
 
 AS
