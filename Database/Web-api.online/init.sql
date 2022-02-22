c979f3da93103e74d74074e07cf67a46b4972947
0takan <hordienko30@gmail.com>
2022-02-22 13:43:46 +0200

    GetRefferers_Paged.sql proc and updated init.sql

--- Database/Web-api.online/init.sql
+++ Database/Web-api.online/init.sql
@@ -590,6 +590,37 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+CREATE PROCEDURE [dbo].[GetRefferers_Paged]
+@page int,
+@pageSize int
+AS
+BEGIN
+
+SELECT 
+anu.Email,
+anu.UserName,
+ui.FullName,
+ui.ReffererId
+
+
+FROM AspNetUsers as anu
+LEFT JOIN UsersInfo as ui
+ON anu.Id = ui.UserId
+WHERE ui.ReffererId IS NOT NULL
+ORDER By anu.Id
+
+OFFSET @pageSize * (@page - 1) ROWS
+FETCH  NEXT @pageSize ROWS ONLY
+
+END
+
+
+
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE PROCEDURE [dbo].[GetRegistratedUsers_Paged]
 @page int,
 @pageSize int
