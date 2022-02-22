ec215571670784814731ebe77530765261fb2890
0takan <hordienko30@gmail.com>
2022-02-22 13:48:43 +0200

    GetCountUserRefferer.sql proc and updated init.sql

--- Database/Web-api.online/init.sql
+++ Database/Web-api.online/init.sql
@@ -528,6 +528,21 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+CREATE PROCEDURE [dbo].[GetCountUserRefferer]
+AS
+BEGIN
+
+SELECT COUNT(1) FROM AspNetUsers as anu
+LEFT JOIN UsersInfo as ui
+ON anu.Id = ui.UserId
+WHERE ui.ReffererId IS NOT NULL
+
+END
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 create PROCEDURE [dbo].[GetDPDCitiesTop10]
 @text nvarchar(50)
 AS
