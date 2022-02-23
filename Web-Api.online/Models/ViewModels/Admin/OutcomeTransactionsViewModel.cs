4d502ffa9a749f2c66479343f6f524a1dc86ab1b
0takan <hordienko30@gmail.com>
2022-02-23 14:57:36 +0200

    Updated ViewModels

--- Web-Api.online/Models/ViewModels/Admin/OutcomeTransactionsViewModel.cs
+++ Web-Api.online/Models/ViewModels/Admin/OutcomeTransactionsViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
 using Web_Api.online.Models.StoredProcedures;
+using Web_Api.online.Models.Tables;
 
 namespace Web_Api.online.Models.ViewModels
 {
     public class OutcomeTransactionsViewModel
     {
-        public List<spGetOutcomeTransactions_Paged> OutcomeTransactions { get; set; }
+        public List<OutcomeTransactionTableModel> OutcomeTransactions { get; set; }
         public PageViewModel PageViewModel { get; set; }
     }
 }
