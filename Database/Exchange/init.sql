23587d55e6cebb3bdaa139e49dc42cc98982b123
Matvey D <mathewsun@rambler.ru>
2022-03-26 01:21:32 +0300

    add stored procedure GetAllPairs.sql

--- Database/Exchange/init.sql
+++ Database/Exchange/init.sql
@@ -940,6 +940,26 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+CREATE PROCEDURE [dbo].[GetAllPairs]
+AS
+BEGIN
+
+SELECT 
+[Id]
+,[Currency1]
+,[Currency2]
+,[Order]
+,[Created]
+,[Header]
+FROM [Exchange].[dbo].[Pairs]
+
+
+END
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE PROCEDURE [dbo].[GetAllTransfers]
 
 AS
