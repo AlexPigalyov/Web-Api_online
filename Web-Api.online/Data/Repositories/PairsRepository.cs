f4dcc1727cebced5142f74c1b8f6d84366da14f3
AlexPigalyov <alexpigalev@nd6tech.com>
2022-03-27 23:52:15 +0300

    add creation new procedures and tables for new crypt currency pair

--- Web-Api.online/Data/Repositories/PairsRepository.cs
+++ Web-Api.online/Data/Repositories/PairsRepository.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Web_Api.online.Models.Tables;
 
@@ -21,9 +23,27 @@ namespace Web_Api.online.Data.Repositories
         public async Task<List<PairsTableModel>> GetAllPairsAsync()
         {
 
-            List<PairsTableModel> result = (List<PairsTableModel>)await _db.QueryAsync<PairsTableModel>("GetAllPairs", commandType: CommandType.StoredProcedure);
+            List<PairsTableModel> result =
+                (List<PairsTableModel>) await _db.QueryAsync<PairsTableModel>("GetAllPairs",
+                    commandType: CommandType.StoredProcedure);
 
             return result;
         }
+
+        public async Task Create(string cryptExchangePairName)
+        {
+            var x = Directory.GetDirectories("Data\\Templates");
+            foreach (var fullname in Directory.GetDirectories("Data\\Templates").OrderByDescending(x => x))
+            {
+                foreach (var file in Directory.GetFiles(fullname))
+                {
+                    await _db
+                        .ExecuteAsync((await File.ReadAllTextAsync(file))
+                            .Replace("{cryptocurrencypair}", cryptExchangePairName));
+                }
+            }
+                
+        }
+    
     }
 }
