b0b2494a1edac316dd86c4ccb8d53d15215cb1a3
Matvey D <mathewsun@rambler.ru>
2022-05-15 23:37:55 +0300

    add proc Get_OpenOrders_ByUserId

--- Database/Exchange/init.sql
+++ Database/Exchange/init.sql
@@ -1898,6 +1898,53 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+CREATE PROCEDURE [dbo].[Get_OpenOrders_ByUserId]
+@createUserId nvarchar(450)
+AS
+BEGIN
+
+SELECT *, 1 as IsBuy, 'BTCUSDT' as Pair FROM [Exchange].[dbo].[BTC_USDT_OpenOrders_Buy]
+WHERE CreateUserId = @createUserId
+UNION
+SELECT *, 0 as IsBuy, 'BTCUSDT' as Pair FROM [Exchange].[dbo].[BTC_USDT_OpenOrders_Sell]
+WHERE CreateUserId = @createUserId
+
+UNION
+SELECT *, 1 as IsBuy, 'DASHUSDT' as Pair FROM [Exchange].[dbo].[DASH_USDT_OpenOrders_Buy]
+WHERE CreateUserId = @createUserId
+UNION
+SELECT *, 0 as IsBuy, 'DASHUSDT' as Pair FROM [Exchange].[dbo].[DASH_USDT_OpenOrders_Sell]
+WHERE CreateUserId = @createUserId
+
+UNION
+SELECT *, 1 as IsBuy, 'DOGEUSDT' as Pair FROM [Exchange].[dbo].[DOGE_USDT_OpenOrders_Buy]
+WHERE CreateUserId = @createUserId
+UNION
+SELECT *, 0 as IsBuy, 'DOGEUSDT' as Pair FROM [Exchange].[dbo].[DOGE_USDT_OpenOrders_Sell]
+WHERE CreateUserId = @createUserId
+
+UNION
+SELECT *, 1 as IsBuy, 'ETHUSDT' as Pair FROM [Exchange].[dbo].[ETH_USDT_OpenOrders_Buy]
+WHERE CreateUserId = @createUserId
+UNION
+SELECT *, 0 as IsBuy, 'ETHUSDT' as Pair FROM [Exchange].[dbo].[ETH_USDT_OpenOrders_Sell]
+WHERE CreateUserId = @createUserId
+
+UNION
+SELECT *, 1 as IsBuy, 'LTCUSDT' as Pair FROM [Exchange].[dbo].[LTC_USDT_OpenOrders_Buy]
+WHERE CreateUserId = @createUserId
+UNION
+SELECT *, 0 as IsBuy, 'LTCUSDT' as Pair FROM [Exchange].[dbo].[LTC_USDT_OpenOrders_Sell]
+WHERE CreateUserId = @createUserId
+
+ORDER BY CreateDate DESC
+
+END
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE PROCEDURE [dbo].[Get_Top10000_Exceptions]
 
 AS
