743e2f77813f3715cdcd211ae3952f3fbb4a0945
Matvey D <mathewsun@rambler.ru>
2022-07-04 04:04:31 +0300

    add fields to Pairs, update page Trade

--- Web-Api.online/Models/Tables/PairsTableModel.cs
+++ Web-Api.online/Models/Tables/PairsTableModel.cs
@@ -11,6 +11,19 @@ namespace Web_Api.online.Models.Tables
         public DateTime Created { get; set; }
         public string Header { get; set; }
         public string Acronim { get; set; }
+
+        public decimal Price { get; set; }
+        public DateTime PriceUpdateDate { get; set; }
+
+        public decimal Change24h { get; set; }
+        public DateTime Change24hUpdateDate { get; set; }
+
+        public decimal Change1h { get; set; }
+        public DateTime Change1hUpdateDate { get; set; }
+
+        public decimal Volume24h { get; set; }
+        public DateTime Volume24hUpdateDate { get; set; }
+
         public string SQLTableName => Currency1 + "_" + Currency2;
         public string ImgUrl { get; set; }
     }
