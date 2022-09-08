f97aaa7ef1db4cbbcb1a894c040b19323b442cab
EDV <edv-work@yandex.ru>
2022-09-08 22:09:51 +0300

    Fix decimals at Wallets, Send and Withdraw

---
+++ Web-Api.online/Extensions/DecimalExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Web_Api.online.Extensions
+{
+    public static class DecimalExtensions
+    {
+        // Precision after comma in decimal
+        private const int Precision = 2;
+
+        /// <summary>
+        /// Converts decimal like 10000,00000000 to 10000,00 or 10000 to 10000
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>Converted decimal to formatted string</returns>
+        public static string ShowAsCurrency(this decimal value)
+        {
+            var stringValue = value.ToString();
+
+            var commaIndex = stringValue.IndexOf(',');
+
+            if (commaIndex < 0) return stringValue;
+
+            return stringValue.Remove(commaIndex + 1 + Precision);
+        }
+    }
+}
