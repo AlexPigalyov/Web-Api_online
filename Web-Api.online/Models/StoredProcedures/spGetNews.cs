f37ac8581d8652575a805480c81a3fe05497a390
MYCAR\Daddy <eoll730@gmail.com>
2022-09-14 19:09:42 +0300

    Add News Page

---
+++ Web-Api.online/Models/StoredProcedures/spGetNews.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Web_Api.online.Models.StoredProcedures
+{
+    public class spGetNews
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public DateTime Created { get; set; }
+        public string AuthorId { get; set; }
+        public string UserName { get; set; }
+    }
+}
