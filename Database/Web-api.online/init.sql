8806d2f84e1c1ca5d4086a331510a72ac42b518b
ecats <ecatsonline@gmail.com>
2022-09-17 19:42:22 +0000

    update GetUserRefferals_Paged procedure

--- Database/Web-api.online/init.sql
+++ Database/Web-api.online/init.sql
@@ -932,17 +932,18 @@ BEGIN
 --set @userId = '08d803ba-a9fb-430e-a0b9-d4a366aeaee7'
 
 
-SELECT 
-LEFT(anu.Email, 2) + '*****' + RIGHT(anu.Email,4) Email,
-ui.ReffererId,
-ui.RegistrationDate
-
+SELECT
+anu.Id
+,LEFT(anu.Email, 2) + '*****' + RIGHT(anu.Email,4) Email
+,ui.ReffererId
+,ui.RegistrationDate
+,anuref.Id
 
 FROM AspNetUsers as anu
-LEFT JOIN UsersInfo as ui
-ON anu.Id = ui.UserId
-WHERE ui.ReffererId = @userId
-ORDER By anu.Id
+LEFT JOIN UsersInfo as ui ON anu.Id = ui.UserId
+LEFT JOIN AspNetUsers anuref ON anuref.Number = ui.ReffererId
+WHERE anuref.Id = @userId
+ORDER By ui.RegistrationDate desc
 
 OFFSET @pageSize * (@page - 1) ROWS
 FETCH  NEXT @pageSize ROWS ONLY
