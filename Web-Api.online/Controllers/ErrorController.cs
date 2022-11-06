d3393b4b74428ecf3bb59849f6c7823df078601d
Matthew <mathew.dubrovin@gmail.com>
2022-11-06 05:31:15 +0300

    add Controllers/ErrorController.cs

---
+++ Web-Api.online/Controllers/ErrorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web_Api.online.Data.Repositories.Abstract;
+using Web_Api.online.Data.Repositories;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Web_Api.online.Models.ViewModels;
+
+namespace Web_Api.online.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ErrorController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PageNotExist()
+        {
+            
+            return View();
+        }
+    }
+}
