4995fca56509fc66efad565891856a227bc0bc57
Matthew <mathew.dubrovin@gmail.com>
2022-11-13 16:19:44 +0300

    fix stats/orders for non BTCUSDT pairs

--- Web-Api.online/wwwroot/js/statsordershub.js
+++ Web-Api.online/wwwroot/js/statsordershub.js
@@ -6,9 +6,15 @@ connection.on("ReceiveMessage", function (recieveModel) {
     //var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     //var encodedMsg = user + " says " + msg;
 
-    let model = JSON.parse(recieveModel);
-    console.log(model);
-    loadOrders(model);
+    var paramCryptocurrency = getUrlParametr('Pair');
+
+    if (paramCryptocurrency == '' || paramCryptocurrency == 'BTCUSDT')
+    {
+        let model = JSON.parse(recieveModel);
+        //console.log(model);
+        loadOrders(model);
+    }
+    
 });
 
 connection.start();
