49515f0f269c2fe25baaa7aa82fa8ae5889ff3c3
Matthew <mathew.dubrovin@gmail.com>
2022-12-13 04:19:33 +0300

    update RegisteredUsersTableModel

--- Web-Api.online/Models/RegistratedUsersTableModel.cs
+++ Web-Api.online/Models/RegistratedUsersTableModel.cs
@@ -7,6 +7,7 @@ namespace Web_Api.online.Models.ViewModels
         public int Number { get; set; }
         public string Email { get; set; }
         public string FullName { get; set; }
-        public DateTime RegistrationDate { get; set; }
+        public DateTime? RegistrationDate { get; set; }
+        public DateTime? Created { get; set; }
     }
 }
